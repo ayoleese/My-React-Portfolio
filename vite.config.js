7ba9c14f210cadd49efa4282b7f1da855f7f62ec
L.lar <l.lar1109@gmail.com>
2024-03-10 17:15:16 -0400

    add different port

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3000,
-//     open: true
-//   }
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 3001,
+    open: true
+  }
 })
+
