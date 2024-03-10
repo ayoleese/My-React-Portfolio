2c8ba81d3ddc1aafaaaf72da844921a40abd5b42
L.lar <l.lar1109@gmail.com>
2024-03-10 01:16:33 -0500

    completed homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
+import React from 'react';
+import profilePic from '../images/profilePic.jpeg'; // Import profile picture
+
 const styles = {
     card: {
         textAlign: 'center',
-        margin: '14rem'
+        width: '68%',
+        margin: '14rem auto',
+        display: 'flex', // Using flexbox for layout
+        alignItems: 'center'
     },
     baseText: {
         fontSize: '4rem'
@@ -11,15 +17,29 @@ const styles = {
         fontSize: '6rem',
         fontWeight: 'bold',
     },
+    profileImageContainer: {
+        marginLeft: '25%' // Adjust the margin to separate the image from text
+    },
+    profileImage: {
+        width: '75%', // width of the image
+        height: '75%', // height of the image
+        borderRadius: '50%', // round up the photo
+        opacity: 0.7, // Lighten up the photo
+    }
 };
 
 export default function Home() {
     return (
-        <div style={styles.card} className="col-sm-3">
-            <text style={styles.baseText}>Hi! I'm </text>
-                <text style={styles.innerText}>Lisa</text>
-            <text style={styles.baseText}>, a web developer</text>
-            <p>Full Stack Developer</p>
+        <div style={styles.card}>
+            <div>
+                <span style={styles.baseText}>Hi! I'm </span>
+                <span style={styles.innerText}>Lisa</span>
+                <span style={styles.baseText}>, a web developer</span>
+                <p>Full Stack Developer</p>
+            </div>
+            <div style={styles.profileImageContainer}>
+                <img src={profilePic} alt="Profile" style={styles.profileImage} />
+            </div>
         </div>
     );
 }
